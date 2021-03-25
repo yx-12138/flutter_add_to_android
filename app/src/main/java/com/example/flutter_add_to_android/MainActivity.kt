116327efe58466b199591984cc6a28febb072f78
yx <1642583417@qq.com>
2021-03-25 10:18:22 +0800

    init  a

--- app/src/main/java/com/example/flutter_add_to_android/MainActivity.kt
+++ app/src/main/java/com/example/flutter_add_to_android/MainActivity.kt
@@ -7,21 +7,37 @@ import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.android.FlutterActivityLaunchConfigs
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.embedding.engine.FlutterEngineCache
+import io.flutter.embedding.engine.dart.DartExecutor
 
 class MainActivity : AppCompatActivity() {
-
+    lateinit var flutterEngine : FlutterEngine
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        flutterEngine = FlutterEngine(this)
+        // Configure an initial route.
+        flutterEngine.navigationChannel.setInitialRoute("/");
+        // Start executing Dart code to pre-warm the FlutterEngine.
+        flutterEngine.dartExecutor.executeDartEntrypoint(
+            DartExecutor.DartEntrypoint.createDefault()
+        )
+        // Cache the FlutterEngine to be used by FlutterActivity or FlutterFragment.
+        FlutterEngineCache
+            .getInstance()
+            .put("my_engine_id", flutterEngine)
+
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
 
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+
+        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener {
             startActivity(
                 FlutterActivity
-                    .withNewEngine()
-                    .initialRoute("/my_route")
+                    .withCachedEngine("my_engine_id")
+                    .backgroundMode(FlutterActivityLaunchConfigs.BackgroundMode.transparent)
                     .build(this)
             )
         }
